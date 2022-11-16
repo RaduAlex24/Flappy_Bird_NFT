6e75a9bbf60a4a8fcbb422bcb580a57e89b83068
RaduAlex24 <radualexandru18@stud.ase.ro>
2022-11-16 19:54:03 +0200

    Simple Transaction

--- Flappy Bird NFT/Assets/Web3Unity/Scripts/Prefabs/WebGL/WebGLSendTransactionExample.cs
+++ Flappy Bird NFT/Assets/Web3Unity/Scripts/Prefabs/WebGL/WebGLSendTransactionExample.cs
@@ -2,16 +2,21 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using TMPro;
 
-#if UNITY_WEBGL
-public class WebGLSendTransactionExample : MonoBehaviour
-{
-    async public void OnSendTransaction()
-    {
+
+public class WebGLSendTransactionExample : MonoBehaviour {
+
+
+    public TextMeshProUGUI hashText;
+
+
+    async public void OnSendTransaction() {
         // account to send to
-        string to = "0x428066dd8A212104Bc9240dCe3cdeA3D3A0f7979";
+        string to = "0xD518ba2e2147f16de82F5d39708125693701033C";
         // amount in wei to send
-        string value = "12300000000000000";
+        string value = "50000000000000000";
         // gas limit OPTIONAL
         string gasLimit = "";
         // gas price OPTIONAL
@@ -20,9 +25,20 @@ public class WebGLSendTransactionExample : MonoBehaviour
         try {
             string response = await Web3GL.SendTransaction(to, value, gasLimit, gasPrice);
             Debug.Log(response);
-        } catch (Exception e) {
+            hashText.text = response;
+        }
+        catch (Exception e) {
             Debug.LogException(e, this);
         }
     }
+
+
+
+    private void Update() {
+        if (hashText.text.Contains("0")) {
+            SceneManager.LoadScene(2);
+        }
+    }
+
+
 }
-#endif
